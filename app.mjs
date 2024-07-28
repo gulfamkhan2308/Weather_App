20e584d46070aad4d17445d19f2e80b3df5c2c14
gulfamkhan2308 <gulfamkhan2308@gmail.com>
2024-07-28 16:02:18 +0500

    updated css

--- app.mjs
+++ app.mjs
@@ -1,7 +1,8 @@
 let getWeather = () => {
     let cityName = document.getElementById("cityName").value;
    // Make a request for a user with a given ID
-axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=752000152e9b5eb2f5e0b8162973cb40&q=${cityName}`)
+   let url = `https://api.openweathermap.org/data/2.5/weather?units=metric&appid=752000152e9b5eb2f5e0b8162973cb40&q=${cityName}`
+axios.get(url)
 .then(function (response) {
   // handle success
   console.log("response is success");
@@ -9,7 +10,17 @@ axios.get(`https://api.openweathermap.org/data/2.5/weather?units=metric&appid=75
   document.getElementById("weather").innerHTML = 
   
   `<h1>${response.data.name}</h1>;
-  <h2>${response.data.main.temp}°C</h2>`;
+  <h2>temp:${response.data.main.temp}°C</h2>
+  <h2> feels_like${response.data.main.feels_like}°C</h2>
+  <h3>weather:${response.data.weather[0].description}</h3>
+  <h3>humidity:${response.data.main.humidity}%</h3>
+  <h3>wind:${response.data.wind.speed}m/s</h3>
+  <h3>pressure:${response.data.main.pressure}hPa</h3>
+  <h3>sea_level${response.data.main.sea_level}m</h3>
+  <h3>grnd_level${response.data.main.grnd_level}m</h3>
+  <h3>temp_max${response.data.main.temp_max}°C</h3>
+  <h3> temp_min${response.data.main.temp_min}°C</h3>
+   `;    
 })
 .catch(function (error) {
   // handle error
